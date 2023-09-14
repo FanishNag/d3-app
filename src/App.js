cf7fd8cfb565429553ee04213cc375c0b3f3e225
Fanish Nag <81068910+FanishNag@users.noreply.github.com>
2023-09-14 14:36:01 +0530

    pie chart

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import BarChart from './Components/BarChart';
 import LineChart from './Components/LineChart';
+import PieChart from './Components/PieChart';
 
 function App() {
   return (
     <div className="App">
       <LineChart/>
       <BarChart/>
+      <PieChart/>
     </div>
   );
 }
