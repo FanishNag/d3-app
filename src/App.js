56caea0d5cf9a6f4d61e6bc759038797a5dca0c1
Fanish Nag <81068910+FanishNag@users.noreply.github.com>
2023-09-18 14:29:41 +0530

    header integration

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import AnimatedBarChart from './Components/AnimatedBarChart';
 import AnimatedLineChart from './Components/AnimatedLineChart';
 import BarChart from './Components/BarChart';
 import BarChartTooTip from './Components/BarChartToolTip';
+import Header from './Components/Header';
 import InteractivePieChart from './Components/InteractivePieChart';
 import LineChart from './Components/LineChart';
 import LineChartToolTip from './Components/LineChartToolTip';
@@ -11,14 +12,17 @@ import PieChart from './Components/PieChart';
 function App() {
   return (
     <div className="App">
-      <LineChart/>
-      <BarChart/>
-      <PieChart/>
-      <AnimatedLineChart/>
-      <AnimatedBarChart/>
-      <LineChartToolTip/>
-      <BarChartTooTip/>
-      <InteractivePieChart/>
+      <Header header={'Simple Charts'}/>
+        <LineChart/>
+        <BarChart/>
+        <PieChart/>
+      <Header header={'Animated Chart'}/>
+        <AnimatedLineChart/>
+        <AnimatedBarChart/>
+      <Header header={'Interactive Chart'}/>
+        <LineChartToolTip/>
+        <BarChartTooTip/>
+        <InteractivePieChart/>
     </div>
   );
 }
---
+++ src/Components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = ({header}) => {
+  return (
+    <div style={{width:'100%', fontSize:20}}>
+        {header}
+    </div>
+  )
+}
+
+export default Header
