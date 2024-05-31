73e722b30c9b583d9522d9b6681cbce2894fab47
Fanish Nag <81068910+FanishNag@users.noreply.github.com>
2024-05-31 15:42:11 +0530

    data file updated

--- src/App.js
+++ src/App.js
@@ -16,42 +16,8 @@ import PieChart from './Components/PieChart';
 import StackedAreaChart from './Components/StackedAreaChart';
 import UltimateLineChart from './Components/UltimateLineChart';
 import marketData from './assets/data.json';
+import { DataLineChart, DataLineChart2, stackedData } from './assets/data';
 function App() {
-  
-  const DataLineChart2 = [
-    {date: 0, amount : 250},
-    {date: 1, amount : 350},
-    {date: 2, amount : 150},
-    {date: 3, amount : 850},
-    {date: 4, amount : 1650},
-    {date: 5, amount : 750},
-    {date: 6, amount : 850},
-    {date: 7, amount : 550},
-    {date: 8, amount : 1050},
-    {date: 9, amount : 450},
-  ]
-  const DataLineChart = [
-    {date: new Date("Thu, 01 Sep 2023"), amount : 250},
-    {date: new Date("Thu, 02 Sep 2023"), amount : 350},
-    {date: new Date("Thu, 03 Sep 2023"), amount : 150},
-    {date: new Date("Thu, 04 Sep 2023"), amount : 850},
-    {date: new Date("Thu, 05 Sep 2023"), amount : 1650},
-    {date: new Date("Thu, 06 Sep 2023"), amount : 750},
-    {date: new Date("Thu, 07 Sep 2023"), amount : 850},
-    {date: new Date("Thu, 08 Sep 2023"), amount : 550},
-    {date: new Date("Thu, 09 Sep 2023"), amount : 1050},
-    {date: new Date("Thu, 10 Sep 2023"), amount : 450},
-  ]
-
-  const stackedData=[
-    { "date": "2023-01-01", "categoryA": 30, "categoryB": 20, "categoryC": 50 },
-    { "date": "2023-02-01", "categoryA": 40, "categoryB": 30, "categoryC": 60 },
-    { "date": "2023-03-01", "categoryA": 35, "categoryB": 25, "categoryC": 45 },
-    { "date": "2023-04-01", "categoryA": 50, "categoryB": 40, "categoryC": 70 },
-    { "date": "2023-05-01", "categoryA": 55, "categoryB": 45, "categoryC": 80 }
-  ]
-  
-  
   return (
     <div className="App">
       <Header header={'Line Charts'}/>
